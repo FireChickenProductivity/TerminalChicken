34587e906a8b64f9e681521b85e3f796da1dc60f
Fire Chicken <firechickenproductivity@gmail.com>
2024-12-20 12:03:48 -0700

    Work on windows support

--- source/terminal_files.py
+++ source/terminal_files.py
@@ -8,14 +8,28 @@ if not os.path.exists(MAIN_TERMINAL_FILE_PATH):
     with open(MAIN_TERMINAL_FILE_PATH, 'w') as _:
         pass
 
-from talon import actions, Module
+from talon import actions, Module, Context
 
 module = Module()
 @module.action_class
 class Actions:
+    def terminal_chicken_open_vscode_workbench():
+        """Opens the vscode workbench"""
+        actions.key("ctrl-p")
+
     def terminal_chicken_open_main_file():
         '''Opens the main terminal chicken file'''
-        actions.key('cmd-p')
+        actions.user.terminal_chicken_open_vscode_workbench()
         actions.insert(MAIN_TERMINAL_FILE_PATH)
         actions.sleep(1)
         actions.key('enter')
+
+mac_context = Context()
+mac_context.matches = r"""
+os: mac
+"""
+@mac_context.action_class('user')
+class MacActions:
+    def terminal_chicken_open_vscode_workbench():
+        """Opens the vscode workbench"""
+        actions.key('cmd-p')
